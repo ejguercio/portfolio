d8d6ec80884f79d2c04dfd8d2870018cbf64298a
Edgar Juan Guercio <jguercio@live.com.ar>
2023-11-09 01:22:10 -0300

    FEAT logos tecnologias

--- front-end/src/components/Technologies/Technologies.jsx
+++ front-end/src/components/Technologies/Technologies.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import css from "./Technologies.module.css"
+import css from './Technologies.module.css';
 
 const Technologies = () => {
   return (
     <section id="technologies" className={css.technologies}>
-      <div className={css.container}>
-        <h2>Tecnologías que utilizo</h2>
-        
+      <h2 className={css.title}>Tecnologias que utilizo</h2>
+      <div className={css.grid}>
+        <div className={css.card}>
+          <box-icon type='logo' name='html5'></box-icon>
+        </div>
+        <div className={css.card}>
+          <box-icon name='css3' type='logo' ></box-icon>
+        </div>
+        <div className={css.card}>
+          <box-icon name='javascript' type='logo' animation='tada' ></box-icon>
+        </div>
+        <div className={css.card}>
+          <box-icon name='nodejs' type='logo' animation='tada' ></box-icon>
+        </div>
+        <div className={css.card}>
+          <box-icon name='react' type='logo' animation='tada' ></box-icon>
+        </div>
+        <div className={css.card}>
+          <box-icon name='postgresql' type='logo' animation='tada' ></box-icon>
+        </div>
       </div>
     </section>
   );
