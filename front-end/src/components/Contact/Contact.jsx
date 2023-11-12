904a37ad2daf910f60f898a1fc8781a94ef4d16b
Edgar Juan Guercio <jguercio@live.com.ar>
2023-11-12 16:45:24 -0300

    FEAT estilos y links de contacto

--- front-end/src/components/Contact/Contact.jsx
+++ front-end/src/components/Contact/Contact.jsx
@@ -2,17 +2,29 @@ import css from "./Contact.module.css"
 
 const Contact = () => {
     return (
-      <section id="contact" className={css.container}> 
-        <div className={css.container}>
+      <section id="contact" className={css.section} > 
           <h2 className={css.title}>Contacto</h2>
-            <box-icon name='mail-send' color='#ffffff' />
-          <span>Esribime: <a href="mailto:ejguercio@gmail.com">ejguercio@gmail.com</a></span>
-            <box-icon name='phone-call' type='solid' color='#ffffff'/>
-          <span>Lamame: <a href="https://wa.me/+543534115272">+543535272</a></span>
-          <box-icon name='linkedin-square' type='logo' color='#ffffff' />
-          <span>Conecta <a href="https://www.linkedin.com/in/ejguercio/" target="_blank" rel="noopener noreferrer">LinkedIn</a></span>
-          <box-icon name='github' type='logo' color='#ffffff' />
-          <span>Seguime <a href="https://www.linkedin.com/in/ejguercio/" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+        <div className={css.container}>
+          <div>
+            <a href="mailto:ejguercio@gmail.com">
+              <box-icon name='mail-send' color='#ffffff' />
+              <span> ejguercio@gmail.com</span>
+            </a>
+            <a href="https://www.linkedin.com/in/ejguercio/" target="_blank" rel="noopener noreferrer">
+              <box-icon name='linkedin-square' type='logo' color='#ffffff' />
+              <span>LinkedIn</span>
+            </a>
+          </div>
+          <div>
+            <a  href="https://wa.me/+543534115272" target="_blank" rel="noopener noreferrer">
+              <box-icon name='phone-call' type='solid' color='#ffffff'/>
+              <span>+543534115272</span>
+            </a>
+            <a href="https://github.com/ejguercio" target="_blank" rel="noopener noreferrer">
+              <box-icon name='github' type='logo' color='#ffffff' />
+              <span>GitHub</span>
+            </a>
+          </div>
         </div>
       </section>
     );
