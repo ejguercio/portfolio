86a8c5b746c56814d416d9c99b6baee16d06f5af
Edgar Juan Guercio <jguercio@live.com.ar>
2024-03-13 22:44:51 -0300

    EDIT buttons and title order

--- src/components/Projects/Dogs/Dogs.jsx
+++ src/components/Projects/Dogs/Dogs.jsx
@@ -5,10 +5,10 @@ const Dogs=()=>{
     return (
         <div className={css.card}>
             <div className={css.cardVideo}>
+                <h3 className={css.title}>Dogs SPA</h3>
                 <iframe src="https://player.vimeo.com/video/854832419?h=f8fa799104" width="640" height="360" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
                 </div>
             <div className={css.cardInfo}>
-                <h3 className={css.title}>Dogs SPA</h3>
                 <p className={css.description}>Página donde consumimos <a href="https://www.thedogapi.com/">The Dog API</a> para acceder a una gran cantidad de información sobre distintas razas de perros con la posibilidad también de agregar razas personalizadas a la base de datos.</p>
                 <div className={css.cardButtons}>
                     <button>Deploy</button>
